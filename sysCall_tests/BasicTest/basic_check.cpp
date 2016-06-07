fd93493bd1f7161cae157285e6393b63546cffc6
Betsalel Williamson <bhw7@pitt.edu>
2016-06-07 10:38:51 -0400

    updated read test

--- sysCall_tests/BasicTest/basic_check.cpp
+++ sysCall_tests/BasicTest/basic_check.cpp
@@ -32,19 +32,33 @@ TEST(BasicTest, Write_to_zeroth_framebuffer){
     ASSERT_EQ(bytesWritten, 1);
 }
 
+#define BUFFER_SIZE 1024
+
 TEST(BasicTest, Read_from_zeroth_framebuffer){
     int filedesc = open("/dev/fb0", O_RDONLY);
     printf("Opened filedesc: %d\n", filedesc);
 
-    void *readbuffer = (char*) malloc(1024);
+    unsigned char *readbuffer = (unsigned char*) malloc(BUFFER_SIZE);
 
     ssize_t bytesRead;
-    if ((bytesRead = read(filedesc,readbuffer,1024)) < 0){
+    if ((bytesRead = read(filedesc,(void *)readbuffer,BUFFER_SIZE)) < 0){
         printf("Bytes read: %d\n", (int) bytesRead);
         FAIL() << "Failed to read";
     }
 
-    ASSERT_EQ(bytesRead, 1024);
+    ASSERT_EQ(bytesRead, BUFFER_SIZE);
     printf("Bytes read: %d\n", (int) bytesRead);
-    printf("%s\n", (char *) readbuffer);
+
+    int ii;
+
+    // printf("0x"); -- uncomment if you want to start with "0x"
+
+    for(ii = 0; ii < BUFFER_SIZE; ii++) {
+        printf("%02x", (unsigned int)(readbuffer[ii]));
+        if(ii%4 == 3) printf(" ");    // groups of 8: makes more readable
+        // uncomment if you want "all one line"
+        if(ii%32 == 31) printf("\n"); // ditto
+    }
+
+    printf("\n");
 }

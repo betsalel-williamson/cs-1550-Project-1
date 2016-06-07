0026501098f1a19f01e4cfa7d34cdd08827896d0
Betsalel Williamson <bhw7@pitt.edu>
2016-06-07 10:05:34 -0400

    Added framebuffer unit tests

--- sysCall_tests/BasicTest/basic_check.cpp
+++ sysCall_tests/BasicTest/basic_check.cpp
@@ -2,10 +2,42 @@
 // Created by Merlin on 16/11/15.
 //
 
+#include <fcntl.h>
 #include "gtest/gtest.h"
 
-TEST(BasicTest, TrueTest ){
-    EXPECT_EQ(true,true);
+TEST(BasicTest, Get_Zeroth_Framebuffer){
+    // opening this file will work
+
+    int filedesc = open("/dev/fb0", O_WRONLY | O_APPEND);
+    printf("Opened filedesc: %d", filedesc);
+
+    ASSERT_GT(filedesc,0);
+}
+
+TEST(BasicTest, Write_To_Zeroth_Framebuffer){
+
+    int filedesc = open("/dev/fb0", O_WRONLY | O_APPEND);
+
+    if(filedesc < 0){
+        FAIL();
+    }
+
+    int writebuffer[1] = {1};
+    ssize_t bytesWritten;
+
+    if((bytesWritten = write(filedesc,(void *) writebuffer, 1)) != 1)
+    {
+        FAIL();
+    }
+    ASSERT_EQ(bytesWritten, 1);
+
+    int readbuffer[1] = {0};
+    ssize_t bytesRead;
+    if ((bytesRead = read(filedesc,readbuffer,1)) != 1){
+        FAIL();
+    }
+
+    ASSERT_EQ(bytesRead, 1);
 }
 
 

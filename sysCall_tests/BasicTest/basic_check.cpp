77cd8af4016d04f3c17137da29fc0c805d48c7c0
Betsalel Williamson <bhw7@pitt.edu>
2016-06-07 10:13:33 -0400

    Updated framebuffer unit tests

--- sysCall_tests/BasicTest/basic_check.cpp
+++ sysCall_tests/BasicTest/basic_check.cpp
@@ -5,7 +5,7 @@
 #include <fcntl.h>
 #include "gtest/gtest.h"
 
-TEST(BasicTest, Get_Zeroth_Framebuffer){
+TEST(BasicTest, Open_zeroth_framebuffer){
     // opening this file will work
 
     int filedesc = open("/dev/fb0", O_WRONLY | O_APPEND);
@@ -14,7 +14,7 @@ TEST(BasicTest, Get_Zeroth_Framebuffer){
     ASSERT_GT(filedesc,0);
 }
 
-TEST(BasicTest, Write_To_Zeroth_Framebuffer){
+TEST(BasicTest, Write_to_zeroth_framebuffer){
 
     int filedesc = open("/dev/fb0", O_WRONLY | O_APPEND);
 
@@ -30,13 +30,17 @@ TEST(BasicTest, Write_To_Zeroth_Framebuffer){
         FAIL() << "Failed to write";
     }
     ASSERT_EQ(bytesWritten, 1);
+}
+
+TEST(BasicTest, Read_from_zeroth_framebuffer){
+    int filedesc = open("/dev/fb0", O_WRONLY | O_APPEND);
+    printf("Opened filedesc: %d", filedesc);
 
     int readbuffer[1] = {0};
     ssize_t bytesRead;
-    if ((bytesRead = read(filedesc,readbuffer,1)) != 1){
+    if ((bytesRead = read(filedesc,readbuffer,1)) == -1){
         FAIL() << "Failed to read";
     }
 
     ASSERT_EQ(bytesRead, 1);
 }
-

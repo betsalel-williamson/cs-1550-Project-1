2b6e6b09908905f5f442aed3e4799906bc57f06e
Betsalel Williamson <bhw7@pitt.edu>
2016-06-08 12:23:00 -0400

    Refactor tests

--- lib/src/syscalls.c
+++ lib/src/syscalls.c
@@ -17,7 +17,7 @@ int open_frame_buffer(int options) {
     return open("/dev/fb0", options);
 }
 
-char *read_frame_buffer(size_t buffer_size, off_t offset, off_t pa_offset) {
+unsigned char * read_frame_buffer(size_t buffer_size, off_t offset, off_t pa_offset) {
     int fd = open_frame_buffer(O_RDONLY);
 
     return  mmap(NULL, buffer_size + offset - pa_offset, PROT_READ,

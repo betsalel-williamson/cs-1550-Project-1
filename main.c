e5a6a9f6f961fdf43b42c279bd7fb4f231d1d83f
Betsalel Williamson <bhw7@pitt.edu>
2016-06-08 14:25:25 -0400

    Updated main

--- main.c
+++ main.c
@@ -7,11 +7,14 @@
 
 int main(int argc, char **argv) {
 
-    unsigned short buffer[10] = {0x0123,0x0123,0x0123,0x0123,0x0123,0x0123,0x0123,0x0123,0x0123,0x0123};
+    unsigned short buffer[2000] = {};
+    for (int i = 0; i < 2000; ++i) {
+        buffer[i] = 0x9999;
+    }
 
 //    puts("\033[2J");
 
-    write_to_frame_buffer(buffer, 10);
+    write_to_frame_buffer(buffer, 2000);
 
     return 0;
 }

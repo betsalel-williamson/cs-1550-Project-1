6e9217e8035e498ae8c674e18c383b9e302a8e71
Betsalel Williamson <bhw7@pitt.edu>
2016-06-09 18:02:32 -0400

    Updated header files in driver

--- square.c
+++ square.c
@@ -1,13 +1,16 @@
-typedef unsigned short color_t;
+#include "syscalls.h"
+#include "draw.h"
 
-void clear_screen();
-void exit_graphics();
-void init_graphics();
-char getkey();
-void sleep_ms(long ms);
+//typedef unsigned short color_t;
 
-void draw_pixel(int x, int y, color_t color);
-void draw_rect(int x1, int y1, int width, int height, color_t c);
+//void clear_screen();
+//void exit_graphics();
+//void init_graphics();
+//char getkey();
+//void sleep_ms(long ms);
+//
+//void draw_pixel(int x, int y, color_t color);
+//void draw_rect(int x1, int y1, int width, int height, color_t c);
 
 int main(int argc, char** argv)
 {

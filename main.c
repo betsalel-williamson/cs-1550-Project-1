8d5dcd727e6b3e57b0ba7ef0434286d7551b0a82
Betsalel Williamson <bhw7@pitt.edu>
2016-06-08 20:50:34 -0400

    Updated color test

--- main.c
+++ main.c
@@ -6,22 +6,121 @@
 #include "syscalls.h"
 
 void draw_sample();
+void draw_sample_colors();
 
 int main(int argc, char **argv) {
 
-    draw_sample();
+    draw_sample_colors();
 
     return 0;
 }
 
+# define SAMPLE_BUFFER_SIZE 2000
 void draw_sample() {
-    unsigned short buffer[2000] = {};
+    unsigned short buffer[SAMPLE_BUFFER_SIZE] = {};
     int i;
-    for (i = 0; i < 2000; ++i) {
+    for (i = 0; i < SAMPLE_BUFFER_SIZE; ++i) {
         buffer[i] = rgb(255, 255, 255);
     }
 
 //    puts("\033[2J");
 
-    write_to_frame_buffer(buffer, 2000);
+    write_to_frame_buffer(buffer, SAMPLE_BUFFER_SIZE);
+}
+
+
+# define SAMPLE_COLOR_BUFFER_SIZE 2048
+void draw_sample_colors() {
+    unsigned short buffer[SAMPLE_COLOR_BUFFER_SIZE] = {};
+    int i;
+    for (i = 0; i < SAMPLE_COLOR_BUFFER_SIZE; ++i) {
+        buffer[i] = Black;
+    }
+
+    write_to_frame_buffer(buffer, SAMPLE_COLOR_BUFFER_SIZE);
+    for (i = 0; i < SAMPLE_COLOR_BUFFER_SIZE; ++i) {
+        buffer[i] = Navy;
+    }
+
+    write_to_frame_buffer(buffer, SAMPLE_COLOR_BUFFER_SIZE);
+    for (i = 0; i < SAMPLE_COLOR_BUFFER_SIZE; ++i) {
+        buffer[i] = DarkGreen;
+    }
+
+    write_to_frame_buffer(buffer, SAMPLE_COLOR_BUFFER_SIZE);
+    for (i = 0; i < SAMPLE_COLOR_BUFFER_SIZE; ++i) {
+        buffer[i] = DarkCyan;
+    }
+
+    write_to_frame_buffer(buffer, SAMPLE_COLOR_BUFFER_SIZE);
+    for (i = 0; i < SAMPLE_COLOR_BUFFER_SIZE; ++i) {
+        buffer[i] = Maroon;
+    }
+
+    write_to_frame_buffer(buffer, SAMPLE_COLOR_BUFFER_SIZE);
+    for (i = 0; i < SAMPLE_COLOR_BUFFER_SIZE; ++i) {
+        buffer[i] = Purple;
+    }
+
+    write_to_frame_buffer(buffer, SAMPLE_COLOR_BUFFER_SIZE);
+    for (i = 0; i < SAMPLE_COLOR_BUFFER_SIZE; ++i) {
+        buffer[i] = Olive;
+    }
+
+    write_to_frame_buffer(buffer, SAMPLE_COLOR_BUFFER_SIZE);
+    for (i = 0; i < SAMPLE_COLOR_BUFFER_SIZE; ++i) {
+        buffer[i] = LightGrey;
+    }
+
+    write_to_frame_buffer(buffer, SAMPLE_COLOR_BUFFER_SIZE);
+    for (i = 0; i < SAMPLE_COLOR_BUFFER_SIZE; ++i) {
+        buffer[i] = DarkGrey;
+    }
+
+    write_to_frame_buffer(buffer, SAMPLE_COLOR_BUFFER_SIZE);
+    for (i = 0; i < SAMPLE_COLOR_BUFFER_SIZE; ++i) {
+        buffer[i] = Blue;
+    }
+
+    write_to_frame_buffer(buffer, SAMPLE_COLOR_BUFFER_SIZE);
+    for (i = 0; i < SAMPLE_COLOR_BUFFER_SIZE; ++i) {
+        buffer[i] = Green;
+    }
+
+    write_to_frame_buffer(buffer, SAMPLE_COLOR_BUFFER_SIZE);
+    for (i = 0; i < SAMPLE_COLOR_BUFFER_SIZE; ++i) {
+        buffer[i] = Cyan;
+    }
+
+    write_to_frame_buffer(buffer, SAMPLE_COLOR_BUFFER_SIZE);
+    for (i = 0; i < SAMPLE_COLOR_BUFFER_SIZE; ++i) {
+        buffer[i] = Red;
+    }
+
+    write_to_frame_buffer(buffer, SAMPLE_COLOR_BUFFER_SIZE);
+    for (i = 0; i < SAMPLE_COLOR_BUFFER_SIZE; ++i) {
+        buffer[i] = Magenta;
+    }
+
+    write_to_frame_buffer(buffer, SAMPLE_COLOR_BUFFER_SIZE);
+    for (i = 0; i < SAMPLE_COLOR_BUFFER_SIZE; ++i) {
+        buffer[i] = Yellow;
+    }
+
+    write_to_frame_buffer(buffer, SAMPLE_COLOR_BUFFER_SIZE);
+    for (i = 0; i < SAMPLE_COLOR_BUFFER_SIZE; ++i) {
+        buffer[i] = White;
+    }
+
+    write_to_frame_buffer(buffer, SAMPLE_COLOR_BUFFER_SIZE);
+    for (i = 0; i < SAMPLE_COLOR_BUFFER_SIZE; ++i) {
+        buffer[i] = Orange;
+    }
+
+    write_to_frame_buffer(buffer, SAMPLE_COLOR_BUFFER_SIZE);
+    for (i = 0; i < SAMPLE_COLOR_BUFFER_SIZE; ++i) {
+        buffer[i] = GreenYellow;
+    }
+
+    write_to_frame_buffer(buffer, SAMPLE_COLOR_BUFFER_SIZE);
 }

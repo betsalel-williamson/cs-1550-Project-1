bb06c03d26d342ffd4398922af612c2eaa47f6b7
Betsalel Williamson <bhw7@pitt.edu>
2016-06-09 17:08:34 -0400

    Updated draw pixel test

--- test/sysCallTests/draw_tests.cpp
+++ test/sysCallTests/draw_tests.cpp
@@ -6,13 +6,19 @@
 #include "draw.h"
 #include "color.h"
 #include "syscalls.h"
+#include "mother.h"
+
+#define BYTES_TO_READ 32
 
 TEST(DrawTests, Draw_pixel) {
     // when called to draw pixel at 0,0
     draw_pixel(0, 0, White);
 
+    mother::print_buffer(read_frame_buffer(BYTES_TO_READ), BYTES_TO_READ);
+
     // then pixel is drawn, reading framebuffer will show that something was drawn
     unsigned char * buffer = read_frame_buffer(2);
+
 
     ASSERT_EQ(0xff, buffer[0]);
     ASSERT_EQ(0xff, buffer[1]);

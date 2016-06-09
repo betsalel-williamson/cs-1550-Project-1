349af2f0a5aaf5d3887fd9644a847ae19ffd6afd
Betsalel Williamson <bhw7@pitt.edu>
2016-06-09 17:03:42 -0400

    Updated draw pixel test

--- test/sysCallTests/draw_tests.cpp
+++ test/sysCallTests/draw_tests.cpp
@@ -12,7 +12,7 @@ TEST(DrawTests, Draw_pixel) {
     draw_pixel(0, 0, White);
 
     // then pixel is drawn, reading framebuffer will show that something was drawn
-    unsigned char * buffer = read_frame_buffer(1);
+    unsigned char * buffer = read_frame_buffer(2);
 
     ASSERT_EQ(0xffff, buffer[0]);
 }

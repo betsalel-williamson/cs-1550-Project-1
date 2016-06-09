d61b38df3bf5e02ec939ca5188252f4adb6351c5
Betsalel Williamson <bhw7@pitt.edu>
2016-06-08 21:10:01 -0400

    Updated color test

--- lib/src/syscalls.c
+++ lib/src/syscalls.c
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <time.h>
+#include <errno.h>
 #include "syscalls.h"
 
 void init_graphics() {
@@ -17,7 +18,8 @@ int open_frame_buffer(int options) {
     return open("/dev/fb0", options);
 }
 
-#define MS_TO_SLEEP 1500
+#define MS_TO_SLEEP 500
+
 int write_to_frame_buffer(unsigned short *write_buffer, size_t num_bytes) {
 
     int filedesc = open_frame_buffer(O_WRONLY | O_APPEND);
@@ -38,7 +40,7 @@ int write_to_frame_buffer(unsigned short *write_buffer, size_t num_bytes) {
     return output;
 }
 
-unsigned char * read_frame_buffer_with_offset(size_t buffer_size, off_t offset, off_t pa_offset);
+unsigned char *read_frame_buffer_with_offset(size_t buffer_size, off_t offset, off_t pa_offset);
 
 unsigned char *read_frame_buffer_with_offset(size_t buffer_size, off_t offset, off_t pa_offset) {
     int fd = open_frame_buffer(O_RDONLY);
@@ -50,15 +52,16 @@ unsigned char *read_frame_buffer_with_offset(size_t buffer_size, off_t offset, o
 unsigned char *read_frame_buffer(size_t buffer_size) {
     int fd = open_frame_buffer(O_RDONLY);
 
-    return read_frame_buffer_with_offset(buffer_size,0,0);
+    return read_frame_buffer_with_offset(buffer_size, 0, 0);
 }
 
-void sleep_ms(long ms){
-    struct timespec req={0};
-    time_t sec=(int)(ms/1000);
-    ms=ms-(sec*1000);
-    req.tv_sec=sec;
-    req.tv_nsec=ms*1000000L;
-    while(nanosleep(&req,&req)==-1)
+// implementation from http://cc.byexamples.com/2007/05/25/nanosleep-is-better-than-sleep-and-usleep/
+void sleep_ms(long ms) {
+    struct timespec req = {0};
+    time_t sec = (int) (ms / 1000);
+    ms = ms - (sec * 1000);
+    req.tv_sec = sec;
+    req.tv_nsec = ms * 1000000L;
+    while (nanosleep(&req, &req) == -1 && errno == EINTR)
         continue;
 }

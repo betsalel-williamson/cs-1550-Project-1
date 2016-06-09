5dcf77acf4a9add3c15930b92eb416005d61c2ba
Betsalel Williamson <bhw7@pitt.edu>
2016-06-08 20:23:04 -0400

    Updated color test

--- lib/include/syscalls.h
+++ lib/include/syscalls.h
@@ -29,6 +29,26 @@ void sleep_ms(long ms);// nanosleep
 
 #define rgb(r,g,b) ((((31*(r+4))/255)<<11) | (((63*(g+2))/255)<<5) | ((31*(b+4))/255))
 
+/* some RGB color definitions                                                 */
+#define Black           0x0000      /*   0,   0,   0 */
+#define Navy            0x000F      /*   0,   0, 128 */
+#define DarkGreen       0x03E0      /*   0, 128,   0 */
+#define DarkCyan        0x03EF      /*   0, 128, 128 */
+#define Maroon          0x7800      /* 128,   0,   0 */
+#define Purple          0x780F      /* 128,   0, 128 */
+#define Olive           0x7BE0      /* 128, 128,   0 */
+#define LightGrey       0xC618      /* 192, 192, 192 */
+#define DarkGrey        0x7BEF      /* 128, 128, 128 */
+#define Blue            0x001F      /*   0,   0, 255 */
+#define Green           0x07E0      /*   0, 255,   0 */
+#define Cyan            0x07FF      /*   0, 255, 255 */
+#define Red             0xF800      /* 255,   0,   0 */
+#define Magenta         0xF81F      /* 255,   0, 255 */
+#define Yellow          0xFFE0      /* 255, 255,   0 */
+#define White           0xFFFF      /* 255, 255, 255 */
+#define Orange          0xFD20      /* 255, 165,   0 */
+#define GreenYellow     0xAFE5      /* 173, 255,  47 */
+
 typedef unsigned short color_t;
 
 void draw_pixel(int x, int y, color_t color);
--- test/sysCallTests/color_tests.cpp
+++ test/sysCallTests/color_tests.cpp
@@ -5,43 +5,74 @@
 #include "gtest/gtest.h"
 #include "syscalls.h"
 
-/* some RGB color definitions                                                 */
-#define Black           0x0000      /*   0,   0,   0 */
-#define Navy            0x000F      /*   0,   0, 128 */
-#define DarkGreen       0x03E0      /*   0, 128,   0 */
-#define DarkCyan        0x03EF      /*   0, 128, 128 */
-#define Maroon          0x7800      /* 128,   0,   0 */
-#define Purple          0x780F      /* 128,   0, 128 */
-#define Olive           0x7BE0      /* 128, 128,   0 */
-#define LightGrey       0xC618      /* 192, 192, 192 */
-#define DarkGrey        0x7BEF      /* 128, 128, 128 */
-#define Blue            0x001F      /*   0,   0, 255 */
-#define Green           0x07E0      /*   0, 255,   0 */
-#define Cyan            0x07FF      /*   0, 255, 255 */
-#define Red             0xF800      /* 255,   0,   0 */
-#define Magenta         0xF81F      /* 255,   0, 255 */
-#define Yellow          0xFFE0      /* 255, 255,   0 */
-#define White           0xFFFF      /* 255, 255, 255 */
-#define Orange          0xFD20      /* 255, 165,   0 */
-#define GreenYellow     0xAFE5      /* 173, 255,  47 */
-
-TEST(BasicTest, White) {
+TEST(BasicTest, Black_test) {
     ASSERT_EQ(Black, rgb(0, 0, 0));
+}
+
+TEST(BasicTest, Navy_test) {
     ASSERT_EQ(Navy, rgb(0, 0, 128));
+}
+
+TEST(BasicTest, DarkGreen_test) {
     ASSERT_EQ(DarkGreen, rgb(0, 128, 0));
+}
+
+TEST(BasicTest, DarkCyan_test) {
     ASSERT_EQ(DarkCyan, rgb(0, 128, 128));
+}
+
+TEST(BasicTest, Maroon_test) {
     ASSERT_EQ(Maroon, rgb(128, 0, 0));
+}
+
+TEST(BasicTest, Purple_test) {
     ASSERT_EQ(Purple, rgb(128, 0, 128));
+}
+
+TEST(BasicTest, Olive_test) {
     ASSERT_EQ(Olive, rgb(128, 128, 0));
+}
+
+TEST(BasicTest, LightGrey_test) {
     ASSERT_EQ(LightGrey, rgb(192, 192, 192));
+}
+
+TEST(BasicTest, DarkGrey_test) {
     ASSERT_EQ(DarkGrey, rgb(128, 128, 128));
+}
+
+TEST(BasicTest, Blue_test) {
     ASSERT_EQ(Blue, rgb(0, 0, 255));
+}
+
+TEST(BasicTest, Green_test) {
     ASSERT_EQ(Green, rgb(0, 255, 0));
+}
+
+TEST(BasicTest, Cyan_test) {
     ASSERT_EQ(Cyan, rgb(0, 255, 255));
+}
+
+TEST(BasicTest, Red_test) {
     ASSERT_EQ(Red, rgb(255, 0, 0));
+}
+
+TEST(BasicTest, Magenta_test) {
     ASSERT_EQ(Magenta, rgb(255, 0, 255));
+}
+
+TEST(BasicTest, Yellow_test) {
     ASSERT_EQ(Yellow, rgb(255, 255, 0));
+}
+
+TEST(BasicTest, White_test) {
     ASSERT_EQ(White, rgb(255, 255, 255));
+}
+
+TEST(BasicTest, Orange_test) {
     ASSERT_EQ(Orange, rgb(255, 165, 0));
+}
+
+TEST(BasicTest, GreenYellow_test) {
     ASSERT_EQ(GreenYellow, rgb(173, 255, 47));
 }

a57500912a7abaa268775c29a8ecf17cd5d61384
Betsalel Williamson <bhw7@pitt.edu>
2016-06-09 19:45:54 -0400

    Added x-y conversion tests

--- lib/src/draw.c
+++ lib/src/draw.c
@@ -20,7 +20,29 @@ void draw_pixel(int x, int y, color_t color) {
 
 
 int get_address_from_x_y(int horizontal, int vertical) {
-    return (int) (vertical + (horizontal * get_horizontal_screen_size()));
+    // using index logic.
+    // convert to counting logic
+    horizontal += 1;
+    vertical += 1;
+
+    if (horizontal > get_horizontal_screen_size()) {
+        return -1;
+    }
+
+    if (vertical > get_vertical_screen_size()) {
+        return -1;
+    }
+
+    int address = 0;
+
+    address += horizontal;
+
+    address += vertical * get_horizontal_screen_size();
+
+    // revert to index logic
+    address -= 1;
+
+    return address;
 }
 
 void draw_rect(int x1, int y1, int width, int height, color_t c) {

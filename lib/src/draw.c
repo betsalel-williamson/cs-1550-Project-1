bbc77abfe422f804b8371aba9bae1e867c8999fe
Betsalel Williamson <bhw7@pitt.edu>
2016-06-10 02:38:00 -0400

    Updated text draw

--- lib/src/draw.c
+++ lib/src/draw.c
@@ -84,7 +84,7 @@ void draw_text(int x, int y, const char *text, color_t c) {
             int k;
             for (k = 0; k < width; ++k, ++temp_address) {
 
-                if (byte & (1 << (width-(k+1)))) {
+                if (byte & (1 << k)) {
                     fb[temp_address] = c;
                 }
             }

2acda7dd4b3d4582d5b7f9f1f8d0e6d535517295
Betsalel Williamson <bhw7@pitt.edu>
2016-06-10 15:49:17 -0400

    Uncommented main draw colors

--- main.c
+++ main.c
@@ -30,11 +30,11 @@ int main(int argc, char **argv) {
 
     init_graphics();
 
-//    draw_sample_colors();
+    draw_sample_colors();
 
-//    draw_rect(20, 20, 100, 40, Orange);
-//
-//    sleep_ms(1000);
+    draw_rect(20, 20, 100, 40, Orange);
+
+    sleep_ms(1000);
 
     draw_text(40, 40, "abc", Red);
 
